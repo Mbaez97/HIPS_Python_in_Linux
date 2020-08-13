8cc3a9a8e449fc23e0266c1eae782f78d2389de2
Mbaez97 <57834256+Mbaez97@users.noreply.github.com>
2020-08-12 23:37:25 -0400

    Update modulos_necesarios.py

--- modulos_necesarios.py
+++ modulos_necesarios.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #Modulo para conexion a la base de datos PostGreSQL
-import psycopg2 as pgDB
+import psycopg2
 #import sys
 import re
 import os
@@ -16,7 +16,7 @@ from os import listdir
 import pwd
 import random
 import time
-import commands
+#import commands
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime, timedelta
@@ -29,15 +29,20 @@ mensj['To'] = "marcelobaezparaguay@gmail.com"#En este campo estaria el correo de
 
 #Actualmente estamos buscando un metodo de encriptacion para los datos, mientras utilizamos los datos en crudo
 #Valores para la conexion a la base de datos
-conn_data = {'host':'localhost', 'database':'hips2020baezoh', 'user':'HIPS2020'};
-#Recuperamos la contrasenha encriptada
-os.system("openssl enc -a-256-cbc -d -in /home/marcelojulianbaezferreira/contrasenhas_cifradas/contrasenhaBD.txt.enc -out contrasenhaBD.txt")
-contrasenhaBD = open("contrasenhaBD.txt")
-passwd = contrasenhaBD.read().replace('\n', '')
-contrasenhaBD.close()
-os.system("rm -rf contrasenhaBD.txt")
-conn = psycopg2.connect(host=conn_data['host'], database=conn_data['database'], user=conn_data['user'], password=passwd)
-cursor = conn.cursor()
+os.system("openssl enc -aes-256-cbc -d -in /home/marcelojulianbaezferreira/contrasenhas_cifradas/contrasenha_BD.txt.enc -out contrasenha_BD.txt")
+	
+#Al desencriptar la contraseña debemos ir a la terminal donde estamos corriendo el script en python para poder colocar la contraseña de desencriptacion
+contrasenha_BD = open("contrasenha_BD.txt")
+connpass_BD = contrasenha_BD.read().replace('\n','')#copiamos el contenido del archivo que abrimos exceptuando los saltos y los espacios vacios
+contrasenha_BD.close()#cerramos el archivo
+os.system("rm -rf contrasenha_BD.txt")#Eliminamos la contraseña desencriptada
+	
+conn_data = {'database':'HIPS', 'user':'postgres'}
+conn = psycopg2.connect(host="localhost", dbname=conn_data['database'], user=conn_data['user'], password=connpass_BD)
+cursor=conn.cursor()
+cursor.execute("SELECT version();")
+record=cursor.fetchone()
+print("Te has conectado a - ",record," ")
 #base de datos conectada          
 
           
